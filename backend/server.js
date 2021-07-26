5d44f82c3f73f7c785ec5441d25fdbf1096f59f1
Tero Ankkuri <teroankkuri@gmail.com>
2021-07-26 15:14:01 +0300

    Updated scripts & dependecies, testing

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const path = require("path");
 
 const jwt = require("jsonwebtoken");
 
@@ -28,6 +29,18 @@ const authRouter = require("./routes/auth");
 app.use("/foods", foodsRouter);
 app.use("/users", usersRouter);
 app.use("/auth", authRouter);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/../build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "/../", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("Api running...?");
+  });
+}
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
