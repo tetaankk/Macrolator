c7c212cc37feedd9d1133e6ee64957c8025da8c5
Tero Ankkuri <teroankkuri@gmail.com>
2021-08-10 11:58:43 +0300

    Fixing CORS, again

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,11 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
@@ -30,6 +34,12 @@ app.use("/foods", foodsRouter);
 app.use("/users", usersRouter);
 app.use("/auth", authRouter);
 app.use("/fetch", fetchRouter);
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers");
+  next();
+});
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/../build")));
