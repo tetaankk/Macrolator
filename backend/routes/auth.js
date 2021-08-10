8e1295bedd8d58ee926cde981628ebb67799c42d
Tero Ankkuri <teroankkuri@gmail.com>
2021-08-10 17:43:31 +0300

    Fixing CORS, again

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,5 +1,5 @@
 import express from "express";
-const authRouter = express.Router();
+const router = express.Router();
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
@@ -11,43 +11,48 @@ import User from "../models/user.model.js";
 // @route POST /auth
 // @desc Login / authenticate user
 // @access Public
-authRouter.post("/", (request, response) => {
-  const body = request.body;
-  console.log(body);
-  console.log("asd");
 
-  // Simple validation on both email and password
-  if (!body.email || !body.password) {
-    return response.status(400).json({ msg: "Please fill all fields" });
-  }
+const authRouter = () => {
+  router.post("/", (request, response) => {
+    const body = request.body;
+    console.log(body);
+    console.log("asd");
 
-  // Check if user exists
-  const user = User.findOne({ email: body.email }).then((user) => {
-    if (!user) return response.status(400).json({ msg: "User does not exist" });
-    // Validate password
-    bcrypt.compare(body.password, user.password).then((isMatch) => {
-      if (!isMatch)
-        return response.status(400).json({ msg: "Invalid credentials" });
+    // Simple validation on both email and password
+    if (!body.email || !body.password) {
+      return response.status(400).json({ msg: "Please fill all fields" });
+    }
 
-      const userForToken = {
-        email: user.email,
-        id: user._id,
-      };
+    // Check if user exists
+    const user = User.findOne({ email: body.email }).then((user) => {
+      if (!user)
+        return response.status(400).json({ msg: "User does not exist" });
+      // Validate password
+      bcrypt.compare(body.password, user.password).then((isMatch) => {
+        if (!isMatch)
+          return response.status(400).json({ msg: "Invalid credentials" });
 
-      const token = jwt.sign(userForToken, jwtSecret);
+        const userForToken = {
+          email: user.email,
+          id: user._id,
+        };
 
-      response.status(200).send({ token, email: user.email });
+        const token = jwt.sign(userForToken, jwtSecret);
+
+        response.status(200).send({ token, email: user.email });
+      });
     });
   });
-});
+
+  router.get("/user", auth, (request, response) => {
+    User.findById(request.user.id)
+      .select("-password")
+      .then((user) => res.json(user));
+  });
+};
 
 // @route GET /auth/user
 // @desc Get user data
 // @access Private
-authRouter.get("/user", auth, (request, response) => {
-  User.findById(request.user.id)
-    .select("-password")
-    .then((user) => res.json(user));
-});
 
 export default authRouter;
