c40cc34287d6fd24cf4119439c75ee3d18fdafdd
Tero Ankkuri <teroankkuri@gmail.com>
2021-08-10 11:59:03 +0300

    Fixing CORS, again

--- backend/server.js
+++ backend/server.js
@@ -9,11 +9,7 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-  })
-);
+app.use(cors());
 app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
