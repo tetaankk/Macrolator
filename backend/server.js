e951e3b90e06a2cd4a74d5467ded503613285c48
Tero Ankkuri <teroankkuri@gmail.com>
2021-08-10 14:49:25 +0300

    Fixing CORS, again

--- backend/server.js
+++ backend/server.js
@@ -9,18 +9,18 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-//app.use(cors);
-//app.options("*", cors());
+app.use(cors());
+app.options("*", cors());
 app.use(express.json());
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+/* app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
   next();
-});
+}); */
 
 /* app.use(function (req, res, next) {
   res.setHeader("Access-Control-Allow-Origin", "*");
